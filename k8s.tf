8f52dc554f769cae6298796535fd59d2da99bb62
Nic Jackson <jackson.nic@gmail.com>
2018-08-28 07:42:32 +0100

    Increase cluster size

--- k8s.tf
+++ k8s.tf
@@ -14,7 +14,7 @@ resource "azurerm_kubernetes_cluster" "gophersearch" {
 
   agent_pool_profile {
     name            = "default"
-    count           = 1
+    count           = 2
     vm_size         = "Standard_D1_v2"
     os_type         = "Linux"
     os_disk_size_gb = 30
